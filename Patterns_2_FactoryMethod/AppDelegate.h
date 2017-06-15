a747fc38b953d33191fe3bba0ca5f4457bc4da92
Ильяс <i.almakaev@gmail.com>
2017-06-15 13:20:25 +0700

    Initial Commit

---
+++ Patterns_2_FactoryMethod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_2_FactoryMethod
+//
+//  Created by Ильяс on 15.06.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Patterns_2_FactoryMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns_2_FactoryMethod
+//
+//  Created by Ильяс on 15.06.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
